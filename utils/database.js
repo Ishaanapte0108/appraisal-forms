df8a541144582fb3cb42c5c097580cd0f74d006e
Ishaan Apte <87040843+Ishaanapte0108@users.noreply.github.com>
2023-01-20 19:36:33 +0530

    Delete utils directory

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-const db = mongoose.connect("mongodb://localhost/test2", ()=>{
-  console.log('connected to test2')
-},e => console.log(e))
-
-module.exports = mongoose
-module.exports = db
